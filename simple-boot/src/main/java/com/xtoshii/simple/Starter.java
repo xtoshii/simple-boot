031b1ff0008ecbb95e8054f638880073010fca9e
xtoshii <2548427264@qq.com>
2022-05-22 23:03:58 +0800

    style(format code):

---
+++ simple-boot/src/main/java/com/xtoshii/simple/Starter.java
@@ -0,0 +1,15 @@
+package com.xtoshii.simple;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(value = "com.xtoshii.simple.mapper")
+public class Starter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class, args);
+    }
+
+}
